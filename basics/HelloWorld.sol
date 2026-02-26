cd9f330d75f4e784af11e0b332c8279caf271295
Mrinal Singh <mrinalsingh7000@gmail.com>
2026-02-26 22:24:14 +0530

    Add updateMessage function with onlyOwner access control

--- basics/HelloWorld.sol
+++ basics/HelloWorld.sol
@@ -3,4 +3,18 @@ pragma solidity ^0.8.20;
 
 contract HelloWorld {
     string public message = "Hello Imam BlockHub";
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not owner");
+        _;
+    }
+
+    function updateMessage(string memory _newMessage) public onlyOwner {
+        message = _newMessage;
+    }
 }

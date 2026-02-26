b383241d9215823563aea9a739973763b0c79989
Imam Blockhub <imamcryptoguy@gmail.com>
2026-02-26 14:45:55 -0800

    Add VotingDemo contract with simple voting logic

---
+++ basics/VotingDemo.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract VotingDemo {
+    mapping(string => uint) public votes;
+
+    function vote(string memory candidate) public {
+        votes[candidate] += 1;
+    }
+
+    function getVotes(string memory candidate) public view returns(uint){
+        return votes[candidate];
+    }
+}
